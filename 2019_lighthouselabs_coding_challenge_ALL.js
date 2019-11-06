7855f0cc6d3890a417a9da02378ffdd210e9986f
Paddlepop25 <shecodeshsucodes18@gmail.com>
2019-11-06 19:27:38 +0800

    solved day 6 challenge

--- 2019_lighthouselabs_coding_challenge_ALL.js
+++ 2019_lighthouselabs_coding_challenge_ALL.js
@@ -48,3 +48,19 @@ const chooseStations = stations => {
   return arr;
 };
 
+// Day 6 Solution
+const voterTurnout = function(voter_signatures, voter_ids) {
+  // Code here!
+  if (voter_signatures.length !== voter_ids.length) {
+    return false;
+  } else {
+    for (let i = 0; i < voter_signatures.length; i++) {
+      if (voter_signatures[i] !== voter_ids[i]) {
+        return "FRAUD!";
+      }
+    }
+    return "All clear, we can count the votes!";
+  }
+};
+
+// Day 7 Solution
--- Challenge07.js
+++ Challenge07.js
@@ -1,1 +1,1 @@
-// Day ? Solution
+// Day 7 Solution
