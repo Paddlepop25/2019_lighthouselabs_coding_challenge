503a53efe3625531b05283a5863f25c1a1ac7dc3
Paddlepop25 <shecodeshsucodes18@gmail.com>
2019-11-07 21:23:53 +0800

    solved day 7 challenge

--- 2019_lighthouselabs_coding_challenge_ALL.js
+++ 2019_lighthouselabs_coding_challenge_ALL.js
@@ -64,3 +64,20 @@ const voterTurnout = function(voter_signatures, voter_ids) {
 };
 
 // Day 7 Solution
+const termTopics = interviews => {
+  let smartCity = 0;
+  let artsFunding = 0;
+  let transportation = 0;
+  for (let i = 0; i < interviews.length; i++) {
+    if (interviews[i] === "smart city") {
+      smartCity++;
+    } else if (interviews[i] === "arts funding") {
+      artsFunding++;
+    } else if (interviews[i] === "transportation") {
+      transportation++;
+    }
+  }
+  return [smartCity, artsFunding, transportation];
+};
+
+// Day 8 Solution
--- Challenge07.js
+++ Challenge07.js
@@ -1,1 +1,16 @@
 // Day 7 Solution
+const termTopics = interviews => {
+  let smartCity = 0;
+  let artsFunding = 0;
+  let transportation = 0;
+  for (let i = 0; i < interviews.length; i++) {
+    if (interviews[i] === "smart city") {
+      smartCity++;
+    } else if (interviews[i] === "arts funding") {
+      artsFunding++;
+    } else if (interviews[i] === "transportation") {
+      transportation++;
+    }
+  }
+  return [smartCity, artsFunding, transportation];
+};
